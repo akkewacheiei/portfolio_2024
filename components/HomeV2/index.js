9bfd8a7c4fc8018bfcc92205e1b7f48ee59ab86d
Broy <akkewach.yodsomboon@gmail.com>
2024-03-30 09:16:54 +0700

    Home V2 Starter No Responsive

--- components/HomeV2/index.js
+++ components/HomeV2/index.js
@@ -3,30 +3,27 @@ import styles from "./Styles.module.css";
 function Home() {
   return (
     <div id={styles.home} className="bg-white">
-      <div className="flex justify-center items-center gap-32">
-        <img
-          className="rounded-lg"
-          src={"/images/profile.jpg"}
-          alt="Profile"
-          width={350}
-        ></img>
+      <div className="flex justify-center items-center gap-[128px]">
         <div className="flex flex-col gap-3">
-          <p className="text-[#5781A7] text-3xl">Hello, I'm</p>
-          <p className="text-black text-5xl font-bold">Akkewach Yodsomboon</p>
-          <p
-            className={`text-black text-3xl underline decoration-4 underline-offset-4 decoration-[#4DA9FE]  ${styles.line_1} ${styles.anim_typewriter} h-10`}
-          >
-            Full-Stack Developer
+          <p className="text-black text-5xl font-bold">Hello,</p>
+          <p className="text-black text-5xl font-bold mt-3">
+            I'm <span className="text-[#4E87F6]">Akkewach</span>
           </p>
-          <div className="flex gap-5 mt-8">
-            <button className="bg-[#5781A7] text-white text-lg w-32 h-12">
+          <p className="text-black text-5xl font-bold mt-3 ">
+            Software Developer
+          </p>
+          <div className="flex gap-5 mt-[45px]">
+            <button className="bg-[#4E87F6] rounded-md text-white text-[16px] font-medium w-[134px] h-[40px]">
               Contact
-            </button>
-            <button className="text-[#5781A7] text-lg border-[#5781A7] border-2  w-32 h-12">
-              Resume
             </button>
           </div>
         </div>
+        <img
+          className="rounded-lg"
+          src={"/images/profile2.png"}
+          alt="Profile"
+          width={490}
+        ></img>
       </div>
     </div>
   );
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -2,17 +2,18 @@ import "./style.css";
 
 export default function Navbar() {
   return (
-    <div id="nav" className="bg-black flex justify-between items-center">
-      <div id="logo" className="flex gap-1">
-        <div className="text-[#5781A7] font-bold">{"</>"}</div>
-        <div className="text-white font-bold">AKKEWACH</div>
+    <div id="nav" className="bg-white flex justify-between items-center border-black border-b border-opacity-10">
+      <div id="logo">
+        <div className="text-black font-medium">Akkewach</div>
       </div>
-      <div id="menu" className="flex gap-7">
-        <div className="text-white">Home</div>
-        <div className="text-white">About</div>
-        <div className="text-white">Resume</div>
-        <div className="text-white">Portfolio</div>
-        <div className="text-white">Contact</div>
+      <div id="menu" className="flex gap-10">
+        <div className="text-black font-medium  underline decoration-2 underline-offset-8 decoration-[#4E87F6]">
+          Home
+        </div>
+        <div className="text-black font-medium">About</div>
+        <div className="text-black font-medium">Resume</div>
+        <div className="text-black font-medium">Portfolio</div>
+        <div className="text-black font-medium">Contact</div>
       </div>
     </div>
   );
