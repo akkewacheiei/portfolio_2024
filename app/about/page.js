193a3c5c63825a478a15e9d012b66bcbc7603493
Broy <akkewach.yodsomboon@gmail.com>
2024-04-06 17:08:09 +0700

    add ProfileV2

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,12 @@
 "use client";
 import Profile from "../../components/Profile/index";
+import Skills from "../../components/Skills/index";
 
 export default function Page() {
   return (
     <>
       <Profile />
+      <Skills />
     </>
   );
 }
---
+++ components/Skills/index.js
@@ -0,0 +1,12 @@
+"use client";
+import styles from "./Styles.module.css";
+
+export default function Page() {
+  return (
+    <div className="bg-gray-200">
+      <h1 id={styles.text2} className="text-5xl font-bold w-[100%]">
+        Skills
+      </h1>
+    </div>
+  );
+}
