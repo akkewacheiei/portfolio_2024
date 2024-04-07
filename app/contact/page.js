274d71feee01f6723e095cbfbf98e5e2460a9fad
Broy <akkewach.yodsomboon@gmail.com>
2024-04-07 18:47:07 +0700

    Add text "This page under construction" on Page Incompleted

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,7 +3,14 @@
 export default function Page() {
   return (
     <div className="bg-gray-200 p-[300px]">
-      <p>Contact</p>
+      <article>
+        <h1
+          className="items-center flex justify-center font-bold mb-4 md:text-4xl text-2xl text-center"
+   
+        >
+          This page under construction 🏗️👷
+        </h1>
+      </article>
     </div>
   );
 }
--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,9 +1,15 @@
-
 "use client";
 export default function Page() {
   return (
     <div className="bg-gray-200 p-[300px]">
-      <p>Portfilio</p>
+      <article>
+        <h1
+          className="items-center flex justify-center font-bold mb-4 md:text-4xl text-2xl text-center"
+   
+        >
+          This page under construction 🏗️👷
+        </h1>
+      </article>
     </div>
   );
 }
