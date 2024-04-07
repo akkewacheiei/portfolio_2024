19cb8c2ded2bdcf913b2eb9884bc242844997593
Broy <akkewach.yodsomboon@gmail.com>
2024-04-08 04:00:23 +0700

    edit Responsive Home page

--- components/HomeV2/index.js
+++ components/HomeV2/index.js
@@ -5,29 +5,29 @@ function Home() {
   return (
     <div
       id={styles.home}
-      className="bg-white flex justify-center items-center lg:items-stretch flex-col-reverse lg:flex-row gap-[6%]"
+      className="bg-white flex justify-center items-center lg:items-stretch flex-col lg:flex-row gap-[6%] md:pt-[10%] pt-[25%] p-[5%]"
     >
       <>
-        <div
-          id={styles.content}
-          className="flex flex-col gap-3 items-center md:items-stretch"
-        >
+        <div id={styles.content} className="flex flex-col gap-3 items-stretch">
           <p className="text-black md:text-6xl text-4xl font-bold ">Hi,</p>
-          <p className="text-black md:text-6xl text-4xl font-bold mt-3">
+          <p className="text-black md:text-6xl text-4xl font-bold lg:mt-3">
             I am <span className="text-[#4E87F6]">Akkewach</span>
           </p>
-          <p className="text-black md:text-6xl text-4xl font-bold mt-3 ">
+          <p className="text-black md:text-6xl text-4xl font-bold lg:mt-3 ">
             Software Developer
           </p>
-          <Link  href={"/about"}className="flex gap-5 mt-[45px] mb-[30px] justify-center md:justify-start">
+          <Link
+            href={"/about"}
+            className="flex gap-5 lg:mt-[45px] lg:mb-[30px] justify-start"
+          >
             <button className="bg-[#4E87F6] rounded-md text-white text-[16px] font-medium w-[134px] h-[40px]">
-             Contact
+              Contact
             </button>
           </Link>
-          <>
+          <div className="flex justify-between lg:mt-[auto]">
             <div
               id={styles.social_icons}
-              className="flex gap-[30px] mt-[auto] justify-center lg:justify-start"
+              className="flex lg:flex-row flex-col gap-[30px] justify-center "
             >
               <a href="https://www.facebook.com/akkewach" target="_blank">
                 <img
@@ -54,19 +54,25 @@ function Home() {
                 ></img>
               </a>
             </div>
-          </>
+            <img
+              id={styles.img}
+              className="w-[90%] lg:w-[490px] lg:hidden"
+              src={"/images/profile2.png"}
+              alt="Profile"
+              
+            ></img>
+          </div>
         </div>
       </>
 
       <>
-        <img
-          id={styles.img}
-          className="mb-10"
-          src={"/images/profile2.png"}
-          alt="Profile"
-          width={490}
-        ></img>
-      </>
+      <img
+              id={styles.img}
+              className="mb-10 hidden lg:block"
+              src={"/images/profile2.png"}
+              alt="Profile"
+              width={490}
+            ></img></>
     </div>
   );
 }
