7bee096c5d727cf65b6b68b3c328fe53563919a9
Broy <akkewach.yodsomboon@gmail.com>
2024-06-29 21:38:33 +0700

    refactor code

--- components/Skills/index.js
+++ components/Skills/index.js
@@ -1,7 +1,32 @@
 "use client";
 import styles from "./Styles.module.css";
+import { useState } from "react";
 
 export default function Page() {
+  const frontendSkill = [
+    "Next.js",
+    "React.js",
+    "React Native",
+    "Redux",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TailwindCSS",
+    "Bootstrap",
+  ];
+
+  const backendSkill = [
+    "Node.js",
+    "Express.js",
+    "REST API",
+    "MySQL",
+    "Sequelize",
+    "MongoDB",
+    "Mongoose",
+  ];
+
+  const otherSkill = ["Git", "AWS", "Nginx", "Figma"];
+
   return (
     <div id={styles.skills} className="bg-white mb-10">
       <div className="text-center mb-20  ">
@@ -13,61 +38,31 @@ export default function Page() {
         <section>
           <h2 className="font-bold text-xl">Frontend</h2>
           <div className="flex flex-wrap gap-2">
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Next.js
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              React.js
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              React Native
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Redux
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              HTML
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              CSS
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              JavaScript
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              TailwindCSS
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Bootstrap
-            </span>
+            {frontendSkill.map((item) => (
+              <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
+                {item}
+              </span>
+            ))}
           </div>
         </section>
         <section>
           <h2 className="font-bold mt-5 text-xl">Backend</h2>
           <div className="flex flex-wrap gap-2">
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Node.js
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              MySQL
-            </span>
+            {backendSkill.map((item) => (
+              <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
+                {item}
+              </span>
+            ))}
           </div>
         </section>
         <section>
           <h2 className="font-bold mt-5 text-xl">Other</h2>
           <div className="flex flex-wrap gap-2">
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Git
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              AWS
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Nginx
-            </span>
-            <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
-              Figma
-            </span>
+            {otherSkill.map((item) => (
+              <span className="rounded-full bg-main hover:animate-pulse px-4 py-2 text-center text-sm text-white">
+                {item}
+              </span>
+            ))}
           </div>
         </section>
       </div>
