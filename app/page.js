597395ed88deb6f324f3920c20ad66e8eec54a48
Broy <akkewach.yodsomboon@gmail.com>
2024-07-07 00:44:00 +0700

    ปรับหน้า app หลักกลับเหมือนเดิม

--- app/page.js
+++ app/page.js
@@ -37,7 +37,6 @@ export default function Page() {
 
       <Skills />
 
-
       <div id="experience_heading" className="flex justify-center">
         <span className=" border-[1px] w-[80%] mb-20 mt-10 flex"></span>
       </div>
